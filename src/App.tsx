067a4f5cc2661d68c6b9955ef226c669382801e8
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2024-07-31 20:05:32 +0500

    added фразамs и авторы,

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Fragment, Info, Toggle } from "./component";
 import ToggleContext from "./Context/ToggleContext/ToggleContext";
 import fetchData, { TimeData } from "./api/Time/time";
+import Moon from '../public/icon-night.svg' 
+import Sun from '../public/icon-day.svg' 
 
 const App: React.FC = () => {
   const [data, setData] = useState<TimeData | null>(null);
@@ -28,20 +30,30 @@ const App: React.FC = () => {
   }, []);
 
   if (!data || !currentTime) {
-    return <div>Loading...</div>;
-  }
+
+    return (
+    <>
+    <div role="status" className="flex justify-center items-center mt-[15rem] text-[40px]">
+    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+</div>
+    <div className="flex justify-center text-[#222222] items-center  text-[40px]">   Loading...</div> </>
+  )}
 
   const currentHour = currentTime.getHours();
   const currentMinute = currentTime.getMinutes();
   const formattedMinute = String(currentMinute).padStart(2, '0');
 
   return (
-    <>
+    <div className={`h-screen ${data.hour <21 ? 'bg-day' : 'bg-night'}`}>
+
       <ToggleContext>
         <Fragment />
         <main className="flex justify-between">
           <section className="ml-[6rem] mt-[4rem]">
-            <h2 className="font-serif mb-[-3.5rem] text-[22px]">GOOD MORNING, IT’S CURRENTLY</h2>
+            <h2 className="font-serif mb-[-3.5rem] text-[22px] flex">{data.hour > 21 ?<><img  src={Moon} alt="moon" /> GOOD EVENING, IT’S CURRENTLY</>:<><img src={Sun} alt="sun" />GOOD MORNING, IT’S CURRENTLY</>}</h2>
             <h1 className="font-bold text-[192px]">
               {currentHour}:{formattedMinute}
               <span className="text-[30px]">BST</span>
@@ -52,7 +64,7 @@ const App: React.FC = () => {
         </main>
         {data && <Info data={{...data, dateTime: currentTime.toISOString()}} />}
       </ToggleContext>
-    </>
+    </div>
   );
 };
 
--- src/component/Fragment/Fragment.tsx
+++ src/component/Fragment/Fragment.tsx
@@ -1,18 +1,27 @@
-import { useTheme } from "../../Context/ToggleContext/ToggleContext"
-
+import  { useState } from 'react';
+import { useTheme } from "../../Context/ToggleContext/ToggleContext";
+import data from '../../api/data.json';
+import Next from '../../../public/restart.svg';
 
 const Fragment = () => {
-   const {isOpen} = useTheme()
+  const { isOpen } = useTheme();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.phrases.length);
+  };
+
   return (
     <>
-    {!isOpen &&
+      {!isOpen && (
+        <section className="w-[650px] h-[85px] pt-[2rem] ml-[6rem] mb-[11rem]">
+          <p className="font-mono text-[18px]">“{data.phrases[currentIndex].quote}”</p>
+          <h5 className="font-bold text-[18px]">{data.phrases[currentIndex].author}</h5>
+          <img className='ml-[40rem] cursor-pointer' onClick={handleNextClick} src={Next} alt="next" />
+        </section>
+      )}
+    </>
+  );
+};
 
-    <section className="w-[650px] h-[85px] mt-[3.5rem] ml-[6rem] mb-[11rem]">
-    <p className="font-mono text-[18px] ">“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
-    <h5 className="font-bold  text-[18px]">Ada Lovelace</h5>
-    </section> }
-   </>
- )
-}
-
-export default Fragment
+export default Fragment;
